e331ce04cf10ec79f50edc987683e93f1af4ad03
Andrew Tridgell <andrew@tridgell.net>
2024-08-21 11:46:14 +1000

    g431 bootloader support

---
+++ Mcu/g431/Src/stm32g4xx_it.c
@@ -0,0 +1,41 @@
+
+#include "stm32g4xx_it.h"
+#include "main.h"
+
+void NMI_Handler(void)
+{
+    while (1) {
+    }
+}
+
+void HardFault_Handler(void)
+{
+    while (1) {
+    }
+}
+
+void MemManage_Handler(void)
+{
+    while (1) {
+    }
+}
+
+void BusFault_Handler(void)
+{
+    while (1) {
+    }
+}
+
+void UsageFault_Handler(void)
+{
+    while (1) {
+    }
+}
+
+void SVC_Handler(void) { }
+
+void DebugMon_Handler(void) { }
+
+void PendSV_Handler(void) { }
+
+void SysTick_Handler(void) { }
---
+++ g431makefile.mk
@@ -0,0 +1,26 @@
+MCU := G431
+PART := STM32G431xx
+
+HAL_FOLDER_$(MCU) := $(HAL_FOLDER)/$(call lc,$(MCU))
+
+MCU_$(MCU) := -mcpu=cortex-m4 -mthumb
+LDSCRIPT_$(MCU) := $(wildcard $(HAL_FOLDER_$(MCU))/*.ld)
+
+SRC_BASE_DIR_$(MCU) := \
+	$(HAL_FOLDER_$(MCU))/Startup \
+	$(HAL_FOLDER_$(MCU))/Drivers/STM32G4xx_HAL_Driver/Src
+
+CFLAGS_$(MCU) := \
+	-I$(HAL_FOLDER_$(MCU))/Inc \
+	-I$(HAL_FOLDER_$(MCU))/Drivers/STM32G4xx_HAL_Driver/Inc \
+	-I$(HAL_FOLDER_$(MCU))/Drivers/CMSIS/Include \
+	-I$(HAL_FOLDER_$(MCU))/Drivers/CMSIS/Device/ST/STM32G4xx/Include
+
+CFLAGS_$(MCU) += \
+	-DHSE_VALUE=8000000 \
+	-D$(PART) \
+	-DUSE_FULL_LL_DRIVER \
+	-DPREFETCH_ENABLE=1
+
+SRC_$(MCU)_BL := $(foreach dir,$(SRC_BASE_DIR_$(MCU)),$(wildcard $(dir)/*.[cs])) \
+	$(wildcard $(HAL_FOLDER_$(MCU))/Src/*.c)
