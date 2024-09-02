689bc74f4076972d641a1b6bc6b44591a7e86f91
Andrew Tridgell <andrew@tridgell.net>
2024-09-02 18:48:25 +1000

    v203: use faster flash programming

--- Mcu/v203/Src/eeprom.c
+++ Mcu/v203/Src/eeprom.c
@@ -8,6 +8,9 @@
 
 #define page_size 0x100 // 256 byte pages for v203
 
+// buffer for FLASH_ProgramPage_Fast
+static uint32_t flash_buffer[page_size/4];
+
 bool save_flash_nolib(const uint8_t* data, uint32_t length, uint32_t add)
 {
     if ((add & 0x03) != 0 || (length & 0x03) != 0 || (length+(add&0xFF)) > page_size) {
@@ -19,22 +22,20 @@ bool save_flash_nolib(const uint8_t* data, uint32_t length, uint32_t add)
 
     FLASH_Unlock();
 
-    // erase page if on page boundary
-    if ((add % page_size) == 0) {
-        // fast erase is 256 bytes at a time, normal Flash_Erasepage
-        // is 4k at a time
-        FLASH_ErasePage_Fast(add);
-    }
+    const uint8_t page_offset = add & 0xFFU;
+    const uint32_t page_base = add & ~0xFFU;
 
-    const uint32_t data_length = length/4;
-    uint32_t index = 0;
-    while (index < data_length) {
-        // flash one word at a time
-        uint32_t word;
-        memcpy((void*)&word, &data[index*4], sizeof(word));
-        FLASH_ProgramWord(add+index*4, word);
-        index++;
-    }
+    // get existing data
+    memcpy(flash_buffer, (void*)page_base, page_size);
+
+    // overwrite with new data
+    memcpy(&flash_buffer[page_offset/4], data, length);
+
+    // fast erase is 256 bytes at a time, normal Flash_Erasepage
+    // is 4k at a time
+    FLASH_ErasePage_Fast(page_base);
+
+    FLASH_ProgramPage_Fast(page_base, flash_buffer);
 
     FLASH_Lock();
 
