0681193e99353bbe3c3003c5cfdb5d107274ccfd
Andrew Tridgell <andrew@tridgell.net>
2024-09-04 13:45:10 +1000

    v203: fixed eeprom fast flash code

--- Mcu/v203/Src/eeprom.c
+++ Mcu/v203/Src/eeprom.c
@@ -6,22 +6,26 @@
 #include <string.h>
 #include <ch32v20x.h>
 
+// #pragma GCC optimize("O1")
+
 #define page_size 0x100 // 256 byte pages for v203
 
-// buffer for FLASH_ProgramPage_Fast
-static uint32_t flash_buffer[page_size/4];
+static void wait_not_busy(void)
+{
+    while ((FLASH->STATR & FLASH_STATR_BSY) != 0) {}
+}
 
 bool save_flash_nolib(const uint8_t* data, uint32_t length, uint32_t add)
 {
     if ((add & 0x03) != 0 || (length & 0x03) != 0 || (length+(add&0xFF)) > page_size) {
-	return false;
+        return false;
     }
 
-    // wait for flash not busy
-    while ((FLASH->STATR & FLASH_STATR_BSY) != 0) {}
+    wait_not_busy();
 
-    FLASH_Unlock();
+    FLASH_Unlock_Fast();
 
+    uint32_t flash_buffer[page_size/4];
     const uint8_t page_offset = add & 0xFFU;
     const uint32_t page_base = add & ~0xFFU;
 
@@ -35,9 +39,13 @@ bool save_flash_nolib(const uint8_t* data, uint32_t length, uint32_t add)
     // is 4k at a time
     FLASH_ErasePage_Fast(page_base);
 
+    wait_not_busy();
+
     FLASH_ProgramPage_Fast(page_base, flash_buffer);
 
-    FLASH_Lock();
+    wait_not_busy();
+
+    FLASH_Lock_Fast();
 
     // ensure data is correct
     return memcmp(data, (const void *)add, length) == 0;
