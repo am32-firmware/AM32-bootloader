ce249dfaec6dbaea472fa76cfce6ad0c1c668c11
Andrew Tridgell <andrew@tridgell.net>
2024-10-08 18:28:20 +1100

    DroneCAN: switched AT32 to PA11/PA12 CAN

--- bootloader/DroneCAN/sys_can_at32.c
+++ bootloader/DroneCAN/sys_can_at32.c
@@ -41,24 +41,25 @@ static void can_gpio_config(void)
 {
     gpio_init_type gpio_init_struct;
 
-    crm_periph_clock_enable(CRM_GPIOB_PERIPH_CLOCK, TRUE);
+    crm_periph_clock_enable(CRM_GPIOA_PERIPH_CLOCK, TRUE);
     crm_periph_clock_enable(CRM_IOMUX_PERIPH_CLOCK, TRUE);
-    gpio_pin_remap_config(CAN1_GMUX_0010,TRUE);
+    gpio_pin_remap_config(CAN1_GMUX_0000,TRUE); // CAN_RX=PA11/CAN_TX=PA12
+    // gpio_pin_remap_config(CAN1_GMUX_0010,TRUE); // CAN_RX=PB8/CAN_TX=PB9
 
     gpio_default_para_init(&gpio_init_struct);
     /* can tx pin */
     gpio_init_struct.gpio_drive_strength = GPIO_DRIVE_STRENGTH_STRONGER;
     gpio_init_struct.gpio_out_type = GPIO_OUTPUT_PUSH_PULL;
     gpio_init_struct.gpio_mode = GPIO_MODE_MUX;
-    gpio_init_struct.gpio_pins = GPIO_PINS_9;
+    gpio_init_struct.gpio_pins = GPIO_PINS_12;
     gpio_init_struct.gpio_pull = GPIO_PULL_NONE;
-    gpio_init(GPIOB, &gpio_init_struct);
+    gpio_init(GPIOA, &gpio_init_struct);
     /* can rx pin */
     gpio_init_struct.gpio_drive_strength = GPIO_DRIVE_STRENGTH_STRONGER;
     gpio_init_struct.gpio_mode = GPIO_MODE_INPUT;
-    gpio_init_struct.gpio_pins = GPIO_PINS_8;
+    gpio_init_struct.gpio_pins = GPIO_PINS_11;
     gpio_init_struct.gpio_pull = GPIO_PULL_UP;
-    gpio_init(GPIOB, &gpio_init_struct);
+    gpio_init(GPIOA, &gpio_init_struct);
 }
 
 /*
