119f3414eb481a8455081cf705461ae719f662e0
Andrew Tridgell <andrew@tridgell.net>
2025-10-29 16:28:49 +1100

    g431: added CANFD driver

--- g431makefile.mk
+++ g431makefile.mk
@@ -19,8 +19,21 @@ CFLAGS_$(MCU) := \
 CFLAGS_$(MCU) += \
 	-DHSE_VALUE=8000000 \
 	-D$(PART) \
+	-DMCU_$(MCU) \
 	-DUSE_FULL_LL_DRIVER \
 	-DPREFETCH_ENABLE=1
 
 SRC_$(MCU)_BL := $(foreach dir,$(SRC_BASE_DIR_$(MCU)),$(wildcard $(dir)/*.[cs])) \
 	$(wildcard $(HAL_FOLDER_$(MCU))/Src/*.c)
+
+# additional CFLAGS and source for DroneCAN
+CFLAGS_DRONECAN_$(MCU) += \
+	-Ibootloader/DroneCAN \
+	-Ibootloader/DroneCAN/libcanard \
+	-Ibootloader/DroneCAN/dsdl_generated/include
+
+SRC_DIR_DRONECAN_$(MCU) += bootloader/DroneCAN \
+		bootloader/DroneCAN/dsdl_generated/src \
+		bootloader/DroneCAN/libcanard
+
+SRC_DRONECAN_$(MCU) := $(foreach dir,$(SRC_DIR_DRONECAN_$(MCU)),$(wildcard $(dir)/*.[cs]))
