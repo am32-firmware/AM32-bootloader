f6c3f269e693961c80da87f3a75215abaad51cd7
Youri Tils <youri.tils@nxp.com>
2026-01-06 09:37:16 +0100

    Updated MCXA153 code so it is able to jump to the main application
    - Wrote low level drivers for GPIO, timers and clock configs
    - Added code to initialize ROM API for flash
    - Added defines in MCXA153 makefile to set EEPROM size and application start address
    - Updated linker script to account for EEPROM space
    - Commented out most code so it is able to jump

--- Mcu/a153/Inc/blutil.h
+++ Mcu/a153/Inc/blutil.h
@@ -9,6 +9,10 @@
 #ifndef BLUTIL_H_
 #define BLUTIL_H_
 
+#include "main.h"
+
+//#define NXP
+
 /*
   8k ram
  */
@@ -16,72 +20,103 @@
 #define RAM_SIZE 8*1024
 
 /*
-  use 32k of flash
+  use 128k of flash
  */
+//#define BOARD_FLASH_SIZE 128
 #define BOARD_FLASH_SIZE 32
 
 //#define GPIO_PIN(n) (1U<<(n))
 
-#define GPIO_PULL_NONE 0
+#define GPIO_PULL_NONE 2
 #define GPIO_PULL_UP   1
-#define GPIO_PULL_DOWN 2
+#define GPIO_PULL_DOWN 0
 
 #define GPIO_OUTPUT_PUSH_PULL 3
+
+flash_config_t s_flashDriver;
 
 //uint32_t SystemCoreClock = 8000000U;
 
 static inline void gpio_mode_set_input(uint32_t pin, uint32_t pull_up_down)
 {
-//  LL_GPIO_SetPinMode(input_port, pin, LL_GPIO_MODE_INPUT);
-//  LL_GPIO_SetPinPull(input_port, pin, pull_up_down);
+	//Check if pull-up/down should be turned off
+	if (pull_up_down == GPIO_PULL_NONE) {
+		//Disable pull-up/down and set to GPIO
+		modifyReg32(&input_port->PCR[pin],
+				PORT_PCR_MUX_MASK | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK,
+				PORT_PCR_MUX(0) | PORT_PCR_PE(0) | PORT_PCR_PS(0));
+	} else {
+		//Enable on pull-up/down and set to GPIO
+		modifyReg32(&input_port->PCR[pin],
+				PORT_PCR_MUX_MASK | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK,
+				PORT_PCR_MUX(0) | PORT_PCR_PE(1) | PORT_PCR_PS(pull_up_down));
+	}
+
+	//Set to input
+	modifyReg32(&input_GPIO->PDDR, (1 << pin), 0);
 }
 
 static inline void gpio_mode_set_output(uint32_t pin, uint32_t output_mode)
 {
-//  LL_GPIO_SetPinMode(input_port, pin, LL_GPIO_MODE_OUTPUT);
-//  LL_GPIO_SetPinOutputType(input_port, pin, output_mode);
+	//Disable pull-up/down and set to GPIO
+	modifyReg32(&input_port->PCR[pin],
+			PORT_PCR_MUX_MASK | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK,
+			PORT_PCR_MUX(0) | PORT_PCR_PE(0) | PORT_PCR_PS(0));
+
+	//Set to output
+	modifyReg32(&input_GPIO->PDDR, 0, (1 << pin));
 }
 
 static inline void gpio_set(uint32_t pin)
 {
-//  LL_GPIO_SetOutputPin(input_port, pin);
+	//Set output to HIGH
+	modifyReg32(&input_GPIO->PSOR, 0, (1 << pin));
 }
 
 static inline void gpio_clear(uint32_t pin)
 {
-//  LL_GPIO_ResetOutputPin(input_port, pin);
+	//Set output to LOW
+	modifyReg32(&input_GPIO->PCOR, 0, (1 << pin));
 }
 
 static inline bool gpio_read(uint32_t pin)
 {
 //  return LL_GPIO_IsInputPinSet(input_port, pin);
-	return 0;
+	return ((input_GPIO->PDIR & (1 << pin)) >> pin);
 }
 
-//#define BL_TIMER TIM2
-
 /*
-  initialise timer for 1us per tick
+ * @brief	Initializes CTIMER2 for use as an interval timer at 1MHz
+ * 			1 tick = 1 us
  */
 static inline void bl_timer_init(void)
 {
-//  LL_TIM_InitTypeDef TIM_InitStruct = {0};
-//
-//  /* Peripheral clock enable */
-//  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM2);
-//
-//  TIM_InitStruct.Prescaler = 47;
-//  TIM_InitStruct.CounterMode = LL_TIM_COUNTERMODE_UP;
-//  TIM_InitStruct.Autoreload = 0xFFFFFFFF;
-//  TIM_InitStruct.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;
-//  LL_TIM_Init(BL_TIMER, &TIM_InitStruct);
-//  LL_TIM_DisableARRPreload(BL_TIMER);
-//  LL_TIM_SetClockSource(BL_TIMER, LL_TIM_CLOCKSOURCE_INTERNAL);
-//  LL_TIM_SetTriggerOutput(BL_TIMER, LL_TIM_TRGO_RESET);
-//  LL_TIM_DisableMasterSlaveMode(BL_TIMER);
-//
-//  LL_TIM_SetCounterMode(BL_TIMER, LL_TIM_COUNTERMODE_UP);
-//  LL_TIM_EnableCounter(BL_TIMER);
+	//Unlock clock configuration registers access
+	modifyReg32(&SYSCON->CLKUNLOCK, SYSCON_CLKUNLOCK_UNLOCK(1), 0);
+
+	//Enable peripheral clocks
+	modifyReg32(&MRCC0->MRCC_GLB_CC0, MRCC_MRCC_GLB_CC0_CTIMER2_MASK, MRCC_MRCC_GLB_CC0_CTIMER2(1));
+
+	//Release peripherals from reset
+	modifyReg32(&MRCC0->MRCC_GLB_RST0, MRCC_MRCC_GLB_RST0_CTIMER2_MASK, MRCC_MRCC_GLB_RST0_CTIMER2(1));
+
+	//Select functional clock for CTIMER2 to FRO_12M clock
+	modifyReg32(&MRCC0->MRCC_CTIMER2_CLKSEL, MRCC_MRCC_CTIMER2_CLKSEL_MUX_MASK, MRCC_MRCC_CTIMER2_CLKSEL_MUX(0));
+
+	//Enable CTIMER2 timer
+	//And set CTIMER2 clock divider to /1
+	modifyReg32(&MRCC0->MRCC_CTIMER2_CLKDIV,
+			MRCC_MRCC_SYSTICK_CLKDIV_DIV_MASK | MRCC_MRCC_SYSTICK_CLKDIV_HALT(1),
+			MRCC_MRCC_SYSTICK_CLKDIV_DIV(0));
+
+	//Freeze clock configuration registers access
+	modifyReg32(&SYSCON->CLKUNLOCK, 0, SYSCON_CLKUNLOCK_UNLOCK(1));
+
+	//Set CTIMER2 prescaler to /6, so clock ticks at 1MHz
+	CTIMER2->PR = 5;
+
+	//Enable CTIMER2
+	modifyReg32(&CTIMER2->TCR, CTIMER_TCR_CEN_MASK, CTIMER_TCR_CEN(1));
 }
 
 /*
@@ -89,13 +124,173 @@ static inline void bl_timer_init(void)
  */
 static inline void bl_timer_disable(void)
 {
-//  LL_TIM_DeInit(BL_TIMER);
+	//Disable CTIMER2
+	modifyReg32(&CTIMER2->TCR, CTIMER_TCR_CEN_MASK, CTIMER_TCR_CEN(0));
 }
 
 static inline uint16_t bl_timer_us(void)
 {
-//  return LL_TIM_GetCounter(BL_TIMER);
-	return 0;
+	return (uint16_t)CTIMER2->TC;
+}
+
+/*
+ * @brief	Initiate system. Enables read/write/execute access for flash.
+ */
+static inline void System_Init(void)
+{
+	SCB->CPACR |= ((3UL << 0*2) | (3UL << 1*2));    /* set CP0, CP1 Full Access in Secure mode (enable PowerQuad) */
+	SCB->NSACR |= ((3UL << 0) | (3UL << 10));   /* enable CP0, CP1, CP10, CP11 Non-secure Access */
+
+	extern void *__Vectors;
+	SCB->VTOR = (uint32_t) &__Vectors;
+
+	/* Enable the LPCAC */
+	SYSCON->LPCAC_CTRL |= SYSCON_LPCAC_CTRL_LPCAC_MEM_REQ_MASK;
+	SYSCON->LPCAC_CTRL &= ~SYSCON_LPCAC_CTRL_DIS_LPCAC_MASK;
+
+	/* Enable flash RWX when FLASH_ACL in IFR0 is invalid */
+	if ((*((volatile const uint32_t *)(0x1000000)) == 0xFFFFFFFFU) ||
+		((*((volatile const uint32_t *)(0x1000000)) == 0x59630000U) &&
+		 (*((volatile const uint32_t *)(0x1000040)) == 0xFFFFFFFFU) &&
+		 (*((volatile const uint32_t *)(0x1000044)) == 0xFFFFFFFFU)))
+	{
+		/* Enable MBC register written with GLIKEY index15 */
+		GLIKEY0->CTRL_0 = 0x00060000U;
+		GLIKEY0->CTRL_0 = 0x0002000FU;
+		GLIKEY0->CTRL_0 = 0x0001000FU;
+		GLIKEY0->CTRL_1 = 0x00290000U;
+		GLIKEY0->CTRL_0 = 0x0002000FU;
+		GLIKEY0->CTRL_1 = 0x00280000U;
+		GLIKEY0->CTRL_0 = 0x0000000FU;
+
+		/* Enable RWX for GLBAC0 */
+		MBC0->MBC_INDEX[0].MBC_MEMN_GLBAC[0] = 0x7700U;
+
+		/* Use GLBAC0 for all flash block */
+		for (uint8_t i = 0; i < 2U; i++)
+		{
+			MBC0->MBC_INDEX[0].MBC_DOM0_MEM0_BLK_CFG_W[i] = 0x00000000U;
+		}
+
+		/* Disable MBC register written */
+		GLIKEY0->CTRL_0 = 0x0002000FU;
+	}
+
+	/* Route the PMC bandgap buffer signal to the ADC */
+	SPC0->CORELDO_CFG |= (1U << 24U);
+
+	uint32_t status = 0;
+	memset(&s_flashDriver, 0, sizeof(flash_config_t));
+
+	//Check if init went successful
+	status = FLASH_API->flash_init(&s_flashDriver);
+	if (status) {
+		__asm volatile ("nop");
+	}
+}
+
+/*
+ * @brief 	Configures the core voltage to 1.1V. Sets the Fast Internal Reference Clock (FIRC) to 192MHz.
+ * 			Sets MUX to select FIRC as MAIN_CLK. Sets system clock divider to /2, so CPU_CLK and SYSTEM_CLK are 96MHz.
+ * 			Also enables SIRC so 12MHz and 1MHz can be sourced to peripherals.
+ */
+static inline void SystemClock_Config(void)
+{
+	//Set VDD_CORE voltage level to 1.1V to set it in Standard Drive mode
+	modifyReg32(&SPC0->ACTIVE_CFG, SPC_ACTIVE_CFG_CORELDO_VDD_LVL_MASK, SPC_ACTIVE_CFG_CORELDO_VDD_LVL(2));
+
+	//Set VDD_CORE drive strength to normal (1.1V)
+	modifyReg32(&SPC0->ACTIVE_CFG, SPC_ACTIVE_CFG_CORELDO_VDD_DS_MASK, SPC_ACTIVE_CFG_CORELDO_VDD_DS(1));
+
+	//Wait for the SPC to finish its transition to 1.1V, i.e. SPC is not busy
+	while ((SPC0->SC & SPC_SC_BUSY_MASK) >> SPC_SC_BUSY_SHIFT) {
+		//Do nothing
+		__asm volatile ("nop");
+	}
+
+	//Set flash memory to support higher voltage level and frequency
+	//This sets the number of additional wait-states.
+	//These need to be set according to the set FIRC frequency:
+	//48MHz = 0
+	//64MHz = 0
+	//96MHz = 1
+	//192MHz = 2
+	//Set to 192MHz
+	modifyReg32(&FMU0->FCTRL, FMU_FCTRL_RWSC_MASK, FMU_FCTRL_RWSC(2));
+
+	//Set SRAM to support higher voltage levels
+	modifyReg32(&SPC0->SRAMCTL, SPC_SRAMCTL_VSM_MASK, SPC_SRAMCTL_VSM(2));
+
+	//Request SRAM voltage update
+	modifyReg32(&SPC0->SRAMCTL, SPC_SRAMCTL_REQ_MASK, SPC_SRAMCTL_REQ(1));
+
+	//Wait for the SRAM voltage change to complete
+	while (!((SPC0->SRAMCTL & SPC_SRAMCTL_ACK_MASK) >> SPC_SRAMCTL_ACK_SHIFT)) {
+		//Do nothing
+		__asm volatile ("nop");
+	}
+
+	//Clear the SRAM voltage update request
+	modifyReg32(&SPC0->SRAMCTL, SPC_SRAMCTL_REQ_MASK, 0);
+
+	//Set System clock divider to 2 to make the CPU and SYSTEM clock 96MHz (this is the max clock) divider value = DIV + 1
+	modifyReg32(&SYSCON->AHBCLKDIV, SYSCON_AHBCLKDIV_DIV_MASK, SYSCON_AHBCLKDIV_DIV(1));
+
+	/* Config FIRC */
+	//Set the Fast Internal Reference Clock (FIRC) to 192MHz
+	modifyReg32(&SCG0->FIRCCFG, SCG_FIRCCFG_FREQ_SEL_MASK, SCG_FIRCCFG_FREQ_SEL(7));
+
+	//Unlock FIRC control status register
+	modifyReg32(&SCG0->FIRCCSR, SCG_FIRCCSR_LK_MASK, 0);
+
+	//Enable FRO_HF clock to peripherals
+	modifyReg32(&SCG0->FIRCCSR, SCG_FIRCCSR_FIRC_FCLK_PERIPH_EN_MASK, SCG_FIRCCSR_FIRC_FCLK_PERIPH_EN(1));
+
+	//Enable FIRC 48MHz clock to peripherals
+	modifyReg32(&SCG0->FIRCCSR, SCG_FIRCCSR_FIRC_SCLK_PERIPH_EN_MASK, SCG_FIRCCSR_FIRC_SCLK_PERIPH_EN(1));
+
+	//Set that FIRC is disabled when in deep sleep mode
+	modifyReg32(&SCG0->FIRCCSR, SCG_FIRCCSR_FIRCSTEN_MASK, SCG_FIRCCSR_FIRCSTEN(0));
+
+	//Wait for the FIRC clock source to be valid
+	while (!((SCG0->FIRCCSR & SCG_FIRCCSR_FIRCVLD_MASK) >> SCG_FIRCCSR_FIRCVLD_SHIFT)) {
+		//Do nothing
+		__asm volatile ("nop");
+	}
+
+	//Select FIRC as MAIN_CLK clock source
+	modifyReg32(&SCG0->RCCR, SCG_RCCR_SCS_MASK, SCG_RCCR_SCS(3));
+
+	//Wait for the MAIN_CLK clock source mux to be set correctly
+	while (((SCG0->CSR & SCG_CSR_SCS_MASK) >> SCG_CSR_SCS_SHIFT) != 3) {
+		//Do nothing
+		__asm volatile ("nop");
+	}
+
+	//Enable FIRC clock source
+	modifyReg32(&SCG0->FIRCCSR, SCG_FIRCCSR_FIRCEN_MASK, SCG_FIRCCSR_FIRCEN(1));
+
+	//Lock FIRC control status register
+	modifyReg32(&SCG0->FIRCCSR, 0, SCG_FIRCCSR_LK_MASK);
+
+	/* Config SIRC */
+	//Unlock SIRC control status register
+	modifyReg32(&SCG0->SIRCCSR, SCG_SIRCCSR_LK_MASK, 0);
+
+	//Set that SIRC is disabled in deep sleep mode
+	modifyReg32(&SCG0->SIRCCSR, SCG_SIRCCSR_SIRCSTEN_MASK, 0);
+
+	//Enable SIRC clock to peripherals
+	modifyReg32(&SCG0->SIRCCSR, SCG_SIRCCSR_SIRC_CLK_PERIPH_EN_MASK, SCG_SIRCCSR_SIRC_CLK_PERIPH_EN(1));
+
+	//Wait for SIRC clock source to be valid
+	while (!((SCG0->SIRCCSR & SCG_SIRCCSR_SIRCVLD_MASK) >> SCG_SIRCCSR_SIRCVLD_SHIFT)) {
+		//Do nothing
+		__asm volatile ("nop");
+	}
+
+	//Lock SIRC control status register
+	modifyReg32(&SCG0->SIRCCSR, 0, SCG_SIRCCSR_LK_MASK);
 }
 
 /*
@@ -103,50 +298,71 @@ static inline uint16_t bl_timer_us(void)
  */
 static inline void bl_clock_config(void)
 {
-//  FLASH->ACR |= FLASH_ACR_PRFTBE;   // prefetch buffer enable
-//
-//  LL_FLASH_SetLatency(LL_FLASH_LATENCY_1);
-//
-//  LL_RCC_HSI_Enable();
-//
-//  /* Wait till HSI is ready */
-//  while (LL_RCC_HSI_IsReady() != 1) ;
-//  LL_RCC_HSI_SetCalibTrimming(16);
-//  LL_RCC_HSI14_Enable();
-//
-//  /* Wait till HSI14 is ready */
-//  while (LL_RCC_HSI14_IsReady() != 1) ;
-//  LL_RCC_HSI14_SetCalibTrimming(16);
-//  LL_RCC_LSI_Enable();
-//
-//  /* Wait till LSI is ready */
-//  while (LL_RCC_LSI_IsReady() != 1) ;
-//  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI_DIV_2, LL_RCC_PLL_MUL_12);
-//  LL_RCC_PLL_Enable();
-//
-//  /* Wait till PLL is ready */
-//  while (LL_RCC_PLL_IsReady() != 1) ;
-//  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
-//  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
-//  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
-//
-//  /* Wait till System clock is ready */
-//  while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL) ;
-//  LL_Init1msTick(48000000);
-//  LL_SetSystemCoreClock(48000000);
+	System_Init();
+	SystemClock_Config();
+	__enable_irq();
 }
 
+/*
+ * @brief 	Enables all GPIO and PORT peripherals
+ */
 static inline void bl_gpio_init(void)
 {
-//  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
-//  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
-//  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
-//
-//  /**/
-//  GPIO_InitStruct.Pin = input_pin;
-//  GPIO_InitStruct.Mode = LL_GPIO_MODE_INPUT;
-//  GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
-//  LL_GPIO_Init(input_port, &GPIO_InitStruct);
+	//Unlock clock configuration registers access
+	modifyReg32(&SYSCON->CLKUNLOCK, SYSCON_CLKUNLOCK_UNLOCK(1), 0);
+
+	//Enable PORT peripheral clocks
+	MRCC0->MRCC_GLB_CC0_SET = MRCC_MRCC_GLB_CC0_PORT0(1);
+	MRCC0->MRCC_GLB_CC0_SET = MRCC_MRCC_GLB_CC0_PORT1(1);
+	MRCC0->MRCC_GLB_CC0_SET = MRCC_MRCC_GLB_CC0_PORT2(1);
+	MRCC0->MRCC_GLB_CC1_SET = MRCC_MRCC_GLB_CC1_PORT3(1);
+
+	//Enable GPIO peripheral clocks
+	MRCC0->MRCC_GLB_CC1_SET = MRCC_MRCC_GLB_CC1_GPIO0(1);
+	MRCC0->MRCC_GLB_CC1_SET = MRCC_MRCC_GLB_CC1_GPIO1(1);
+	MRCC0->MRCC_GLB_CC1_SET = MRCC_MRCC_GLB_CC1_GPIO2(1);
+	MRCC0->MRCC_GLB_CC1_SET = MRCC_MRCC_GLB_CC1_GPIO3(1);
+
+	//Enable INPUTMUX peripheral clock
+	MRCC0->MRCC_GLB_CC0_SET = MRCC_MRCC_GLB_RST0_INPUTMUX0(1);
+
+	//Release PORT peripherals from reset
+	MRCC0->MRCC_GLB_RST0_SET = MRCC_MRCC_GLB_RST0_PORT0(1);
+	MRCC0->MRCC_GLB_RST0_SET = MRCC_MRCC_GLB_RST0_PORT1(1);
+	MRCC0->MRCC_GLB_RST0_SET = MRCC_MRCC_GLB_RST0_PORT2(1);
+	MRCC0->MRCC_GLB_RST1_SET = MRCC_MRCC_GLB_RST1_PORT3(1);
+
+	//Release GPIO peripherals from reset
+	MRCC0->MRCC_GLB_RST1_SET = MRCC_MRCC_GLB_RST1_GPIO0(1);
+	MRCC0->MRCC_GLB_RST1_SET = MRCC_MRCC_GLB_RST1_GPIO1(1);
+	MRCC0->MRCC_GLB_RST1_SET = MRCC_MRCC_GLB_RST1_GPIO2(1);
+	MRCC0->MRCC_GLB_RST1_SET = MRCC_MRCC_GLB_RST1_GPIO3(1);
+
+	//Release INPUTMUX peripheral from reset
+	MRCC0->MRCC_GLB_RST0_SET = MRCC_MRCC_GLB_RST0_INPUTMUX0(1);
+
+	//Freeze clock configuration registers access
+	modifyReg32(&SYSCON->CLKUNLOCK, 0, SYSCON_CLKUNLOCK_UNLOCK(1));
+
+	//Enable GPIO pins for testing/debugging. P3.27, P2.17, P3.28. Set them to output
+//	modifyReg32(&PORT3->PCR[27],	//ENC_A
+//			PORT_PCR_MUX_MASK | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK,
+//			PORT_PCR_MUX(0) | PORT_PCR_PE(1) | PORT_PCR_PS(0));
+//	modifyReg32(&GPIO3->PDDR, 0, (1 << 27));
+//	GPIO3->PCOR = (1 << 27);
+
+	//pin 12, 13 and 14 cannot be used as these are occupied by USB FS on the MCXA14x and MCXA15x
+//	modifyReg32(&PORT2->PCR[17],	//ENC_B
+//			PORT_PCR_MUX_MASK | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK,
+//			PORT_PCR_MUX(0) | PORT_PCR_PE(1) | PORT_PCR_PS(0));
+//	modifyReg32(&GPIO2->PDDR, 0, (1 << 17));
+//	GPIO2->PCOR = (1 << 17);
+
+//	modifyReg32(&PORT3->PCR[28],	//ENC_I
+//			PORT_PCR_MUX_MASK | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK,
+//			PORT_PCR_MUX(0) | PORT_PCR_PE(1) | PORT_PCR_PS(0));
+//	modifyReg32(&GPIO3->PDDR, 0, (1 << 28));
+//	GPIO3->PCOR = (1 << 28);
 }
 
 /*
@@ -154,15 +370,8 @@ static inline void bl_gpio_init(void)
  */
 static inline bool bl_was_software_reset(void)
 {
-//  return (RCC->CSR & RCC_CSR_SFTRSTF) != 0;
+//	return (CMC->SRS & CMC_SRS_SW_MASK);
 	return 0;
-}
-
-/*
-  no need for any action in SystemInit
- */
-void SystemInit()
-{
 }
 
 /*
